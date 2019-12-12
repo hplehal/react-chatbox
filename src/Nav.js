9d517df36a410db28a6b8e100d163155936ef108
Tej Lehal <tejlehal@Tejs-MacBook-Air.local>
2019-12-12 01:01:16 -0500

    seperated and arranged all the component in respective area

---
+++ src/Nav.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { db } from './firebase';
+
+const Nav = () => {
+    const [channels, setChannels] = useState([]);
+
+    useEffect(() => {
+      return db.collection('channels').onSnapshot((snapshot) => {
+        const docs = [];
+        snapshot.forEach((doc => {
+          docs.push({
+            ...doc.data(),
+            id: doc.id
+          })
+        }))
+        setChannels(docs);
+      });
+    },[])
+    
+    return(
+    <div className="Nav">
+        <div className="User">
+            <img
+                className="UserImage"
+                alt="whatever"
+                src="https://placekitten.com/64/64"
+            />
+            <div>
+                <div>Hartej Lehal</div>
+                <div>
+                    <button className="text-button">log out</button>
+                </div>
+            </div>
+        </div>
+        <nav className="ChannelNav">
+            {channels.map(channel =>(
+                <a key={channel.id} href={`channel/${channel.id}`}># {channel.id}</a>
+            ))}
+        </nav>
+    </div>
+    )
+}
+    
+export default Nav;
